84a3624398e0e246073c10f27650bd083fe84285
Alba Cardona <albal347@yahoo.es>
2023-09-18 16:52:14 -0500

    ajustes

--- control/monitor.py
+++ control/monitor.py
@@ -7,6 +7,7 @@ import paho.mqtt.client as mqtt
 import schedule
 import time
 from django.conf import settings
+import dateutil.relativedelta
 
 client = mqtt.Client(settings.MQTT_USER_PUB)
 
@@ -32,8 +33,6 @@ def analyze_data():
                 'station__location__state__name',
                 'station__location__country__name')
     alerts = 0
-    print("DATA: ")
-    print(data)
     for item in aggregation:
         alert = False
 
@@ -51,15 +50,20 @@ def analyze_data():
             if item["check_value"] > max_value or item["check_value"] < min_value:
                 alert = True
         else:
-            batteryLastData = Data.objects.filter(measurement = item['measurement__name']).order_by('-time')[:2]
-            
-            if(item["check_value"] < min_value and batteryLastData[1]["value"] > batteryLastData[0]["value"]):
-                # ALARMA ADICIONAL PARA RECORDAR RECARGAR BATERÍA PORQUE NO LO HA HECHO:
-                message = "ALERT: CHARGE THE BATTERY NOW!"
-                topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
-                print(datetime.now(), "Sending alert to {} suggesting the charge of the battery".format(topic))
-                client.publish(topic, message)
+            if item["check_value"] < min_value:
                 alert = True
+                batteryLastData = Data.objects.filter(measurement = item['measurement__name']).order_by('-time')[:2]
+                print("BATTERY DATA:")
+                print(batteryLastData)
+                if batteryLastData[1]["value"] > batteryLastData[0]["value"]:
+                    # ALARMA ADICIONAL PARA RECORDAR RECARGAR BATERÍA PORQUE NO LO HA HECHO:
+                    print("BATERÍA BAJA Y DESCONECTADA DE LA ENERGÍA")
+                    message = "ALERT: CHARGE THE BATTERY NOW!"
+                    topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
+                    print(datetime.now(), "Sending alert to {} suggesting the charge of the battery".format(topic))
+                    client.publish(topic, message)
+                else:
+                    print("BATERÍA RECARGANDO")
 
         if alert:
             message = "ALERT {} {} {}".format(variable, min_value, max_value)
@@ -123,3 +127,103 @@ def start_cron():
     while 1:
         schedule.run_pending()
         time.sleep(1)
+        
+        
+
+def get_last_context(countryParam,stateParam,cityParam,userParam):
+    """
+Se procesan los datos para cargar el contexto del template.
+El template espera un contexto de este tipo:
+{
+    "data": {
+        "temperatura": {
+            "min": float,
+            "max": float,
+            "avg": float,
+            "data": [
+                (timestamp1, medición1),
+                (timestamp2, medición2),
+                (timestamp3, medición3),
+                ...
+            ]
+        },
+        "variable2" : {min,max,avg,data},
+        ...
+    },
+    "measurements": [Measurement0, Measurement1, ...],
+}
+"""
+    context = {}
+    try:
+        context["data"], context["measurements"] = get_last_week_data(
+            userParam, cityParam, stateParam, countryParam
+        )
+    except Exception as e:
+        print("Error get_context_data. User: " + userParam, e)
+    return context
+
+
+def get_last_week_data(user, city, state, country):
+    result = {}
+    start = datetime.now()
+    start = start - dateutil.relativedelta.relativedelta(days=1)
+    try:
+        userO = User.objects.get(username=user)
+        location = None
+        try:
+            cityO = City.objects.get(name=city)
+            stateO = State.objects.get(name=state)
+            countryO = Country.objects.get(name=country)
+            location = Location.objects.get(
+                city=cityO, state=stateO, country=countryO
+            )
+        except:
+            print("Specified location does not exist")
+        print("LAST_WEEK: Got user and lcoation:",
+              user, city, state, country)
+        if userO == None or location == None:
+            raise "No existe el usuario o ubicación indicada"
+        stationO = Station.objects.get(user=userO, location=location)
+        print("LAST_WEEK: Got station:", user, location, stationO)
+        if stationO == None:
+            raise "No hay datos para esa ubicación"
+        measurementsO = get_measurements()
+        print("LAST_WEEK: Measurements got: ", measurementsO)
+        for measure in measurementsO:
+            print("LAST_WEEK: Filtering measure: ", measure)
+            # time__gte=start.date() Filtro para último día
+            start_ts = int(start.timestamp() * 1000000)
+            raw_data = Data.objects.filter(
+                station=stationO, time__gte=start_ts, measurement=measure
+            ).order_by("-base_time")[:2]
+            print("LAST_WEEK: Raw data: ", len(raw_data))
+            data = []
+            for reg in raw_data:
+                values = reg.values
+                times = reg.times
+                print("Len vals: ", len(values), "Len times: ", len(times))
+                for i in range(len(values)):
+                    data.append(
+                        (
+                            ((reg.base_time.timestamp() +
+                             times[i]) * 1000 // 1),
+                            values[i],
+                        )
+                    )
+
+            minVal = raw_data.aggregate(Min("min_value"))["min_value__min"]
+            maxVal = raw_data.aggregate(Max("max_value"))["max_value__max"]
+            avgVal = sum(reg.avg_value * reg.length for reg in raw_data) / sum(
+                reg.length for reg in raw_data
+            )
+            result[measure.name] = {
+                "min": minVal if minVal != None else 0,
+                "max": maxVal if maxVal != None else 0,
+                "avg": round(avgVal if avgVal != None else 0, 2),
+                "data": data,
+            }
+    except Exception as error:
+        print("Error en consulta de datos:", error)
+        traceback.print_exc()
+
+    return result, measurementsO
